2fe6600798ed3558855a1626c2b6e510175c28ee
Matthias Zobrist <matthias.zobrist@zepi.net>
2026-07-25 14:53:34 +0200

    Adjusted the breadcrumbs

--- src/Twig/ProjectGroupExtension.php
+++ src/Twig/ProjectGroupExtension.php
@@ -4,16 +4,24 @@ namespace Mosparo\Twig;
 
 use Mosparo\Entity\ProjectGroup;
 use Symfony\Component\Form\FormView;
+use Symfony\Component\Routing\RouterInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
 class ProjectGroupExtension extends AbstractExtension
 {
+    protected RouterInterface $router;
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
 
     public function getFunctions(): array
     {
         return [
-            new TwigFunction('find_project_group_form_field', [$this, 'findProjectGroupFormField'])
+            new TwigFunction('find_project_group_form_field', [$this, 'findProjectGroupFormField']),
+            new TwigFunction('get_project_group_breadcrumbs', [$this, 'getProjectGroupBreadcrumbs']),
         ];
     }
 
@@ -33,5 +41,26 @@ class ProjectGroupExtension extends AbstractExtension
         }
 
         return null;
+    }
+
+    public function getProjectGroupBreadcrumbs(ProjectGroup $projectGroup): array
+    {
+        $breadcrumbItems = [
+            [
+                'title' => $projectGroup->getName(),
+                'url' => $this->router->generate('project_list_group', ['projectGroup' => $projectGroup->getId()]),
+            ]
+        ];
+
+        $parent = $projectGroup->getParent();
+        while ($parent != null) {
+            $breadcrumbItems[] = [
+                'title' => $parent->getName(),
+                'url' => $this->router->generate('project_list_group', ['projectGroup' => $parent->getId()]),
+            ];
+            $parent = $parent->getParent();
+        }
+
+        return $breadcrumbItems;
     }
 }
